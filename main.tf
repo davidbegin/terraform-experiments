270c3ff48a0b97a1bc7b4b4cc2680dae01d30c64
davidbegin <davidmichaelbe@gmail.com>
2017-06-08 07:54:23 -0700

    Add a healthy check to ELB with autoscaling

--- main.tf
+++ main.tf
@@ -22,11 +22,14 @@ user_data = <<-EOF
 }
 
 resource "aws_autoscaling_group" "two_to_ten_autoscaling_setup" {
-launch_configuration = "${aws_launch_configuration.ec2_launch_template.id}"
-availability_zones   = ["${data.aws_availability_zones.all.names}"]
+  launch_configuration = "${aws_launch_configuration.ec2_launch_template.id}"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
 
-min_size = 2
-max_size = 10
+  load_balancers    = ["${aws_elb.begins_elb.name}"]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
 
   tag {
     key                 = "Name"
@@ -39,12 +42,21 @@ max_size = 10
 resource "aws_elb" "begins_elb" {
   name               = "terraform-asg-example"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.elb.id}"]
 
   listener {
     lb_port           = 80
     lb_protocol       = "http"
     instance_port     = "${var.server_port}"
     instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout              = 3
+    interval             = 30
+    target               = "HTTP:${var.server_port}/"
   }
 }
 
@@ -66,6 +78,24 @@ resource "aws_security_group" "allow_all_traffic_on_server_port" {
   }
 }
 
+resource "aws_security_group" "elb" {
+  name = "begins-elb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 /* ============================== VARIABLES, DATA AND OUTPUT ============================== */
 
 variable "server_port" {
@@ -77,4 +107,8 @@ data "aws_availability_zones" "all" {}
 
 output "availability_zones" {
   value = "${data.aws_availability_zones.all.names}"
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.begins_elb.dns_name}"
 }
