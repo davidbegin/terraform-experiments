ef42d5a15f399850a83d2fe5ffbc666b9dfabc78
davidbegin <davidmichaelbe@gmail.com>
2017-06-08 07:08:12 -0700

    Add Elastic Load Balancer and Autoscaling Groups
      also some structure to main.tf and removing duplication instance and
      autoscaling server code

--- main.tf
+++ main.tf
@@ -2,15 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "server_port" {
-  description = "port the server is running on"
-  default     = 8080
-}
+/* ============================== SERVERS AND LOAD BALANCERS ============================== */
 
-resource "aws_instance" "example" {
-  ami           = "ami-40d28157"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-40d28157"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.allow_all_traffic_on_server_port.id}"]
+
 
   user_data = <<-EOF
               #!/bin/bash
@@ -18,12 +16,42 @@ resource "aws_instance" "example" {
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
-  tags {
-    Name = "terraform-example"
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-resource "aws_security_group" "instance" {
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones   = ["${data.aws_availability_zones.all.names}"]
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+
+resource "aws_elb" "begins_elb" {
+  name               = "terraform-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_port     = "${var.server_port}"
+    instance_protocol = "http"
+  }
+}
+
+/* ============================== SECURITY GROUPS ============================== */
+
+
+resource "aws_security_group" "allow_all_traffic_on_server_port" {
   name = "terraform-example-instance"
 
   ingress {
@@ -32,8 +60,21 @@ resource "aws_security_group" "instance" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-output "public_ip" {
-  value = "${aws_instance.example.public_ip}"
+/* ============================== VARIABLES, DATA AND OUTPUT ============================== */
+
+variable "server_port" {
+  description = "port the server is running on"
+  default     = 8080
+}
+
+data "aws_availability_zones" "all" {}
+
+output "availability_zones" {
+  value = "${data.aws_availability_zones.all.names}"
 }
